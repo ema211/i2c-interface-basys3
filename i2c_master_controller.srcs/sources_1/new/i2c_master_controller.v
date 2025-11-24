45b13a458caa4726137da451ddaf3a0476024def
Emanuel <jesus.aldana@iteso.mx>
2025-11-24 12:10:21 -0600

    Commit inicial

---
+++ i2c_master_controller.srcs/sources_1/new/i2c_master_controller.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.11.2025 11:56:25
+// Design Name: 
+// Module Name: i2c_master_controller
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module i2c_master_controller(
+
+    );
+endmodule
